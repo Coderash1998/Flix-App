fc3c3953ea888143ea2e3102f084d631d5acf43f
Coderash1998 <47770728+Coderash1998@users.noreply.github.com>
2022-09-23 11:26:06 -0500

    Added new changes

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Vallabh Ashish Kedarisetti on 9/22/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
